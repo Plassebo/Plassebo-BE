7e4f78fdd6a8e0f1d430b8537d4a2324f48668b2
seungheonL <rumah800@naver.com>
2023-07-26 17:44:38 +0900

    이미지 업로드 api 샘플 응답 데이터 추가

--- api-server/app.js
+++ api-server/app.js
@@ -32,7 +32,33 @@ app.post('/images', upload.single('file'), (req, res, next) => {
   console.log('업로드된 파일의 전체 경로 ', path);
   console.log('파일의 바이트(byte 사이즈)', size);
 
-  res.json({ message: 'success' });
+  // TODO: 모델 돌리는 서버 만들어서 관광지 이름 가져오기, 지금은 모델이 '해운대'라고 추론한 것으로 가정
+
+  const attractionName = '해운대';
+
+  // TODO: attractionName 근처에 있는 음식점들 json으로 가져오기
+  // 메모: busan_attraction.json에서 "cat1": "A05" 혹은 "contenttypeid": "39" 는 음식점을 의미
+
+  res.json({
+    attractionName,
+    restaurants: [
+      {
+        name: '미우 숯불갈비',
+        address: '부산 해운대구 구남로 22 2층',
+        phoneNumber: '0507-1389-8983',
+      },
+      {
+        name: '미우 숯불갈비',
+        address: '부산 해운대구 구남로 22 2층',
+        phoneNumber: '0507-1389-8983',
+      },
+      {
+        name: '미우 숯불갈비',
+        address: '부산 해운대구 구남로 22 2층',
+        phoneNumber: '0507-1389-8983',
+      },
+    ],
+  });
 });
 
 app.get('/', (req, res) => {
